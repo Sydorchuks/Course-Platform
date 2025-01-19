ae1f45c5db4ac0b338598dfe8a98ce14b43f128e
Sydorchuks <77363409+Sydorchuks@users.noreply.github.com>
2025-01-19 21:33:12 +0200

    Changed course handling, added couple of files for table systems.

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,21 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
 
-const CoursesPage = () => {
+import { db } from '@/lib/db'
+import React from 'react'
+import { DataTable } from './_components/data-table'
+import { columns } from './_components/columns'
+
+const CoursesPage = async() => {
+  const courses = await db.course.findMany({
+    orderBy:{
+      createdAt:"desc"
+    }
+  })
+
+
   return (
     <div className='p-6'>
-        <Link href="/teacher/create">
-            <Button>
-                New Course
-            </Button>
-        </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
